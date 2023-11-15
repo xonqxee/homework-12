d0a8887afbed6a5a63f463cfa983430e65c37f61
xonqxee <test@ez.com>
2023-11-15 17:09:55 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+$("#addHtmlt").html("<h1>New Element</h1>");
+$(".hide").hide();
+$("section").remove();
+$("#div").ready(function(e){
+    $("#mydiv").addClass("addClass");
+    $("#mydiv").text("Lumos Maxima");
+});
